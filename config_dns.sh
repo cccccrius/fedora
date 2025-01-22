a335823a16eeb4e246088e0ed81869078c3beb5d
cccccrius <crius38@gmail.com>
2025-01-22 13:57:06 +0100

    Update config_dns.sh

--- config_dns.sh
+++ config_dns.sh
@@ -9,7 +9,17 @@ DNSSEC=yes
 FallbackDNS=45.90.28.181 2a07:a8c0::c8:d79a 45.90.30.181 2a07:a8c1::c8:d79a
 EOF
 
-systemctl restart systemd-resolved
+sudo systemctl restart systemd-resolved
+sudo systemctl restart systemd-networkd
 
-resolvectl query torrent9.ing
-dig torrent9.ing
+nmcli -g name,type connection  show  --active | awk -F: '/ethernet|wireless/ { print $1 }' | while read connection
+do
+  nmcli con mod "$connection" ipv6.ignore-auto-dns yes
+  nmcli con mod "$connection" ipv4.ignore-auto-dns yes
+  nmcli con mod "$connection" ipv4.dns "94.140.14.14 94.140.15.15"
+  nmcli con mod "$connection" ipv6.dns "2a10:50c0::ad1:ff 2a10:50c0::ad2:ff"
+  nmcli con down "$connection" && nmcli con up "$connection"
+done
+
+nmcli dev show
+resolvectl status
